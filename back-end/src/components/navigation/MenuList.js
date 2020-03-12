458055187dbb15817b8619e41899bf107eb846e7
Kevin Carter <9323jkc@gmail.com>
2020-03-12 07:14:23 -0700

    Complete Menu Layout

--- back-end/src/components/navigation/MenuList.js
+++ back-end/src/components/navigation/MenuList.js
@@ -2,6 +2,16 @@ import React, { Component, Fragment } from 'react'
 import Breadcrumb from '../common/Breadcrumb';
 import data from '../../assets/data/listMenu';
 import Datatable from '../common/Datatable'
+import ReactTable from 'react-table';
+import { ShoppingBag } from 'react-feather';
+
+
+const menus = [{ title: 'Primary Menu', items: ["Kwik - ShoppingBag", "Recorder", "Stickers", "Decals", "Paper", "Products"] },
+{ title: 'Seconardy Menu', items: ["Item1", "Recorder", "Stickers", "Decals", "Paper", "Products"] },
+{ title: 'Third Menu', items: ["Kwik - ShoppingBag", "Recorder", "Stickers", "Decals", "Paper", "Products"] },
+{ title: 'Footer1', items: ["Kwik - ShoppingBag", "Recorder", "Stickers", "Decals", "Paper", "Products"] },
+{ title: 'Footer2', items: ["Kwik - ShoppingBag", "Recorder", "Stickers", "Decals", "Paper", "Products"] }]
+
 
 export class MenuList extends Component {
     render() {
@@ -17,24 +27,40 @@ export class MenuList extends Component {
                                 </div>
                                 <div className="card-body">
                                     <div className="row">
-                                        <div className="col-md-6 col-lg-3">
+                                        <div className="col-sm-12 col-md-3 ">
                                             <p>Menus, or link lists, help your customers navigate around your online store. </p>
                                             <p>Your store supports 4 menu variations.</p>
                                             <p>You can also create nested menus to display drop-down menus, and group products or pages together.</p>
                                         </div>
-                                        <div className="col-md-6 col-log-9">
-                                            <Datatable
-                                                multiSelectOption={true}
-                                                myData={data}
-                                                pageSize={6}
-                                                pagination={false}
-                                                class="-striped -highlight"
-                                            />
+                                        <div className="col-sm-12 col-md-9 ">
+                                            <div className="container">
+                                                <table className="table table-bordered table-responsive-md">
+                                                    <thead className="thead-white">
+                                                        <tr>
+                                                            <th scope="col">Title</th>
+                                                            <th scope="col">Menu Items</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody style={{ backgroundColor: '#F7F7F9' }}>
+                                                        {
+                                                            menus.map((menu, index) => (
+                                                                <tr key={index}>
+                                                                    <td scope="col"><b>{menu.title}</b></td>
+                                                                    <td scope="col">{menu.items.join(",")}</td>
+                                                                </tr>
+                                                            ))
+                                                        }
+
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <div className="col-sm-12 col-md-4" style={{ marginTop: '50px' }}>
+                                                <button className="btn btn-secondary">ADD Menu</button>
+                                            </div>
+
                                         </div>
                                     </div>
-                                    <div id="batchDelete" className="category-table order-table coupon-list-delete">
 
-                                    </div>
                                 </div>
                             </div>
                         </div>
